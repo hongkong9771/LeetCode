0214430ac31fc5ba680884a04b06efa34bba688c
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-03-01 20:35:39 +0800

    第一次提交

---
+++ Code/001.two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, num in enumerate(nums):
+            if target - num in d:
+                return [d[target - num], i]
+            d[num] = i
+        # no special case handling becasue it's assumed that it has only one solution
+            
