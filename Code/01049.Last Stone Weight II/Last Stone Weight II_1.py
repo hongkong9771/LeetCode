721f2c64f6ad22490aae73f248c1891daf72a464
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-03-26 14:54:53 +0800

    2021.3.26提交

---
+++ Code/01049.Last Stone Weight II/Last Stone Weight II_1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        """
+        题目的思想就是在stones列表中找到几个元素，使得其和接近sum(stones)/2，
+        这样才能满足最后的石头重量最小，当找到的元素和刚好等于sum的一半时，最后没有石头。
+        """
+        l = len(stones)
+        S = sum(stones) // 2  # 向下取整，当sum(stones)
+        # 当stones中存在一个数大于S（stones和的一半）时，最后石头的最小重量如下:
+        if max(stones) > S:
+            return 2 * max(stones) - sum(stones)
+        # dp数组初始化
+        dp = [[0] * (S + 1) for _ in range(l)]
+        for k in range(stones[0], S + 1):
+            dp[0][k] = stones[0]
+
+        # 动态传递过程
+        for i in range(1, l):
+            for j in range(1, S + 1):
+                if stones[i] <= j:
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stones[i]] + stones[i])
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return sum(stones) - dp[l - 1][S] * 2
--- Code/0416.Partition Equal Subset Sum/Partition Equal Subset Sum_2.py
+++ Code/0416.Partition Equal Subset Sum/Partition Equal Subset Sum_2.py
@@ -2,7 +2,7 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         '''
         要满足题意，即从非空数组中选出一部分数组元素，使得这些数组元素的和为数组元素总和的一半
-        二维dp数组表示，从数组下标为0-i的数组中选择部分元素，使得其和为j，若满足，则dp[i][j]=True
+        一维dp数组表示，从数组下标为0-i的数组中选择部分元素，使得其和为j，若满足，则dp[j]=True
         '''
         S = int(sum(nums)/2)
         l = len(nums)
@@ -16,19 +16,19 @@ class Solution:
         if l < 2:
             return False
         # 初始化动态数组
-        dp = [[False]*(S+1) for _ in range(l)]
-        for i in range(l):
-            dp[i][0] = True
-        if nums[0] <= S:
-            dp[0][nums[0]] = True
+        dp = [False]*(S+1)
+        dp[0] = True            # 不选取任何正整数，则被选取的正整数为0
+        dp[nums[0]] = True
 
         # 动态传递
-        for i in range(l):
-            for j in range(1, S+1):
+        for i in range(1, l):
+            for j in range(S, 0, -1):
                 if nums[i] == j:
-                    dp[i][j] = True
+                    dp[j] = True
                 elif nums[i] < j:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
+                    dp[j] = dp[j] or dp[j-nums[i]]
                 else:
-                    dp[i][j] = dp[i-1][j]
-        return dp[l-1][S]
+                    dp[j] = dp[j]
+        return dp[S]
+
+
