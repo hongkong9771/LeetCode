20a1e1dc67917fa838a35adc7e9ea6a6d9c6fba9
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-04-08 16:14:34 +0800

    Update Remove Linked List Elements.py

--- Code/0203.Remove Linked List Elements/Remove Linked List Elements.py
+++ Code/0203.Remove Linked List Elements/Remove Linked List Elements.py
@@ -5,11 +5,11 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        if not head:
-            return head
         # 当要删除的元素为第一个元素时
         while head and head.val == val:
             head = head.next
+        if not head:
+            return head
         else:
             cur = head
             pre = None              # 用于储存被删除节点的上一节点
