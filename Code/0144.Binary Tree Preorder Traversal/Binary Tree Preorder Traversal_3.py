d7bf3d6833365754e975006a3807133c62c9450f
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-04-27 09:52:20 +0800

    Update Binary Tree Preorder Traversal_3.py

--- Code/0144.Binary Tree Preorder Traversal/Binary Tree Preorder Traversal_3.py
+++ Code/0144.Binary Tree Preorder Traversal/Binary Tree Preorder Traversal_3.py
@@ -6,16 +6,24 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
-        # 使用迭代方法求解
+        # 使用栈的思想进行迭代
+        """
+        先将根节点（此处的根节点也可指子树中的根节点，不一定是整棵子树的根节点）放入栈中，
+        然后将根节点进行出栈操作，接着，按顺序将该根节点的右、左子节点进栈
+        """
+        # 用于存储最终的结果，
         res = []
         stack = []
-        while root or len(stack) > 0:
-            # 先添加根节点，然后找到最左端最下面的叶子节点
-            while root:
-                res.append(root.val)
-                stack.append(root)
-                root = root.left
-            if len(stack) > 0:
-                node = stack.pop()
-                root = node.right
+        if root:
+            stack.append(root)
+        cur = root
+        while cur and len(stack) > 0:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right is not None:
+                stack.append(node.right)
+                cur = node.right
+            if node.left is not None:
+                stack.append(node.left)
+                cur = node.left
         return res
