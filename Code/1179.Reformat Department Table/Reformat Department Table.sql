a4e6529c638657456ce9162d7ed586d79d81ca72
HongKong <hongkong9771@outlook.com>
2021-07-04 14:42:18 +0800

    Create Reformat Department Table.sql

---
+++ Code/1179.Reformat Department Table/Reformat Department Table.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+# 聚合函数用于将组数据处理成单一数据
+select id,
+    sum(case month when 'Jan' then revenue end) 'Jan_Revenue',
+    sum(case month when 'Feb' then revenue end) 'Feb_Revenue',
+    sum(case month when 'Mar' then revenue end) 'Mar_Revenue',
+    sum(case month when 'Apr' then revenue end) 'Apr_Revenue',
+    sum(case month when 'May' then revenue end) 'May_Revenue',
+    sum(case month when 'Jun' then revenue end) 'Jun_Revenue',
+    sum(case month when 'Jul' then revenue end) 'Jul_Revenue',
+    sum(case month when 'Aug' then revenue end) 'Aug_Revenue',
+    sum(case month when 'Sep' then revenue end) 'Sep_Revenue',
+    sum(case month when 'Oct' then revenue end) 'Oct_Revenue',
+    sum(case month when 'Nov' then revenue end) 'Nov_Revenue',
+    sum(case month when 'Dec' then revenue end) 'Dec_Revenue'
+from Department
+group by id;
+    
