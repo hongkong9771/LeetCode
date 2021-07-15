4054f41fa3d1cbed20c2c3b2710d1e2868aac679
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-07-15 17:50:30 +0800

    2021.7.15

--- Code/0416.Partition Equal Subset Sum/Partition Equal Subset Sum_1.py
+++ Code/0416.Partition Equal Subset Sum/Partition Equal Subset Sum_1.py
@@ -6,21 +6,21 @@ class Solution:
         '''
         S = int(sum(nums)/2)
         l = len(nums)
-        # 当数组元素的和为奇数时，不满足条件
+        # 1.当数组元素的和为奇数时，不满足条件
         if sum(nums)%2 == 1:
             return False
-        # 当数组中的最大元素大于数组和的一半时，不满足条件
+        # 2.当数组中的最大元素大于数组和的一半时，不满足条件
         if max(nums) > S:
             return False
-        # 当数组元素个数小于2时，无法被分为两个子集，不满足条件
+        # 3.当数组元素个数小于2时，无法被分为两个子集，不满足条件
         if l < 2:
             return False
         # 初始化动态数组
         dp = [[False]*(S+1) for _ in range(l)]
         for i in range(l):
             dp[i][0] = True
-        if nums[0] <= S:
-            dp[0][nums[0]] = True
+        # nums[0]不会超过S，前面第2步已经判断过了
+        dp[0][nums[0]] = True
 
         # 动态传递
         for i in range(1, l):
