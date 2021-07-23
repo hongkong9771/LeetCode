41059337b95ab6ca5b4d583c1dc5e1601355522f
HonngKong <46513782+hongkong9771@users.noreply.github.com>
2021-07-23 11:24:39 +0800

    2021.7.23提交买卖股票的最佳时机

---
+++ Code/0121.Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        dp[i][0]表示第i天持有股票的现金，dp[i][1]表示第i天不持有股票的现金。假设最开始的现金为0元
+        则dp[i][0]可以分成两种情况：
+        1）当天并未买股票，则可能是由前一天购买的股票，所以dp[i][0] = dp[i-1][0]
+        2）当天购买了股票，所以dp[i][0] = -prices[i]
+        dp[i][1]可以分成两种情况：
+        1）当天并未卖出股票，则可能是由前一天卖出的股票，所以dp[i][1] = dp[i-1][1]
+        2）当天卖出了股票，所以dp[i][0] = prices[i] + dp[i-1][0]
+        """
+        l = len(prices)
+        dp = [[0] * 2 for _ in range(l)]
+        # 初始化动态数组
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+
+        # 动态传递
+        for i in range(1,l):
+            dp[i][0] = max(dp[i-1][0], -prices[i])      # 持有股票
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])  # 不持有股票
+        return dp[l-1][1]
