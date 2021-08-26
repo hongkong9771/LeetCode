0cc716ca356a73f76289e44cf673f61c0150a5cb
HongKong <46513782+hongkong9771@users.noreply.github.com>
2021-08-26 17:44:13 +0800

    Update Two Sum_1.py

--- Code/0001.Two Sum/Two Sum_1.py
+++ Code/0001.Two Sum/Two Sum_1.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-    """在第i个数的后面判断是否存在满足条件的数target-nums[i]"""
+    """
+    在第i个数的后面判断是否存在满足条件的数target-nums[i]
+    """
         for i in range(len(nums)):
             if target - nums[i] in nums[i+1:]:
                 return i, nums.index(target-nums[i], i+1)
