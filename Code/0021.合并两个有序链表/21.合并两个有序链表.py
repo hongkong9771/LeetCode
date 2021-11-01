1e5ff05262433a0fd474605519031d2c54405229
HongKong <hongkong9771@outlook.com>
2021-11-01 18:32:57 +0800

    update lc21.合并两个有序链表 2021.11.1

---
+++ Code/0021.合并两个有序链表/21.合并两个有序链表.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        l = ListNode(0)
+        cur = l
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return l.next
