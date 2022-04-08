81140694e13d01604f1e71afa3ef857431334726
HongKong <hongkong9771@outlook.com>
2022-04-08 22:03:45 +0800

    Update ReadMe File 2022.4.8

---
+++ Code/0413.等差数列划分/413. 等差数列划分_1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        """
+        对于一个长度为L的等差数列而言，如果后面接上一个数，得到一个长度为L+1的等差数列，
+        则其对结果的贡献值可由之前长度为L的等差数列的贡献值得来。
+        假设以nums[i]结尾的数的等差数组的子数组个数为dp[i]，一共有以下两种情况：
+        1) 当nums[i]-nums[i-1] = nums[i-1]-nums[i-2]时，dp[i] = dp[i-1] + 1
+        2) 当nums[i]-nums[i-1] != nums[i-1]-nums[i-2]时，dp[i] = 0
+        """
+
+        n = len(nums)
+
+        # 动态数组初始化
+        dp = [0] * n
+        res = 0     # 记录所有的个数
+        # 动态传递过程
+        for i in range(2, n):
+            if nums[i]-nums[i-1] == nums[i-1]-nums[i-2]:
+                dp[i] = dp[i-1] + 1
+            else:   
+                dp[i] = 0   # 此行代码也可删除，写出来知识为了保证代码的可读性与完整性
+            res += dp[i]
+        return res
